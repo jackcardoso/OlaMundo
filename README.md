663bae77f103b5bd77fa74673822b411504d5043
Jack Cardoso <jackcardoso.oliveira@gmail.com>
2021-06-20 15:10:03 -0300

    Meu primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+My first project using Git and GitHub
---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+
+    var ola =  'Olá Mundo!';
+    var elementH1 = document.createElement('h1');
+
+    console.log(ola);
+    window.alert(ola);
+    elementH1.appendChild(document.createTextNode(ola));
+    document.getElementById('container').appendChild(elementH1);
+})

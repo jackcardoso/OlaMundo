9f40fed79a11718dd39e1c3f494bf71204e0a48c
Jack Cardoso <jackcardoso.oliveira@gmail.com>
2021-06-21 00:34:21 -0300

    Adicionando um quadrado e fazendo ele rebater nas laterais em javascript

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
 window.addEventListener('load', function() {
     var ola =  'Olá Mundo!';
-    var elementH1 = document.createElement('h1'); // Cria a tag <h1>
+    var elementH1   = document.createElement('h1'); // Cria a tag <h1>
+    var square      = document.createElement('div');
+    var container   = document.getElementById('container');
+
+    container.style.position = "relative";
+    square.style.position = 'absolute';
+    square.className = 'square';
+    square.style.left = 0;
+    square.style.top = '14em';
 
     console.log(ola);
     window.alert(ola);
+
+
     elementH1.appendChild(document.createTextNode(ola));
-    document.getElementById('container').appendChild(elementH1);
+    container.appendChild(elementH1);
+    container.appendChild(square);
+
+    moveSquare(square);
+
 })
+
+function moveSquare(square){
+    let position = 0;
+    let speed = 1;
+    setInterval(function(){
+        square.style.left = (position+=speed) + 'px';
+        if(position <=0 || position >= (window.innerWidth-square.offsetWidth)){
+            speed *= -1;
+        }
+
+    }, 5);
+    
+}
+
+
+
+
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");   
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.top = pos + "px"; 
+        elem.style.left = pos + "px"; 
+      }
+    }
+  }
